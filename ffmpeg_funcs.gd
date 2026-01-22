b6cdd867eddcf9d89a24b3a312a89b88f2d37dd3
unknown <dasindi100200@gmail.com>
2026-01-22 18:29:47 +0700

    add base funcs to UI, Options, Input file path with arguments and file explorer select

---
+++ ffmpeg_funcs.gd
@@ -0,0 +1,85 @@
+extends Node
+#
+##region Video functions
+#func change_audio_bitrate_in_video(bitrate):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -b:a {self.parameters[bitrate].text}k {abs_file.stem}_audio_compressed{abs_file.suffix}"
+	#OS.system(command)
+#
+#func change_fps(fps):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -r {self.parameters[fps].text} {abs_file.stem}_editfps{abs_file.suffix}"
+	#os.system(command)
+#
+#func extract_audio():
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -vn {abs_file.stem}_extracted.mp3"
+	#os.system(command)
+#
+#func change_bitrate(bitrate):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -b:v {self.parameters[bitrate].text}k {abs_file.stem}_changed_bitrate{abs_file.suffix}"
+	#os.system(command)
+#
+#func convert_video(format):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c copy {abs_file.stem}_converted.{self.parameters[format].text}"
+	#os.system(command)
+#
+#func compress_video_by_size(size):
+	#result = subprocess.run([f"{ffprobe_path}", "-v", "error", "-show_entries",
+							#"format=duration", "-of",
+							#"funcault=noprint_wrappers=1:nokey=1", file],
+		#stdout=subprocess.PIPE,
+		#stderr=subprocess.STDOUT)
+	#duration = float(result.stdout)
+#
+	#filesize = os.path.getsize(file)/1024**2
+#
+	#new_bitrate = ((size * 8192) / duration)-128
+	#print(duration)
+	#print(filesize)
+	#print(new_bitrate)
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -b:v {new_bitrate}k {abs_file.stem}_compressed{abs_file.suffix}"
+	#os.system(command)
+#
+#func resize_video(size):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -vf scale={self.parameters[size].text} {abs_file.stem}_resized{abs_file.suffix}"
+	#os.system(command)
+##endregion
+##region Image functions
+#func convert_image(format):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} {abs_file.stem}.{self.parameters[format].text}"
+	#os.system(command)
+#func resize_image(size):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -s {self.parameters[size].text} {abs_file.stem}_resized{abs_file.suffix}"
+	#os.system(command)
+	#print(command)
+#func compress_image(jpeg_parameter):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -q:v {self.parameters[jpeg_parameter].text} {abs_file.stem}_compressed.jpg"
+	#os.system(command)
+##endregion
+##region Audio functions
+#
+#func convert_audio(format):
+	#command = "echo empty"
+	#if format == "wav":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a pcm_s16le {abs_file.name}.{format}" #Несжатый, высокое
+	#if format == "mp3":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a libmp3lame {abs_file.name}.{format}" #Универсальный
+	#if format == "flac":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a flac -compression_level 8 {abs_file.name}.{format}"# Lossless
+	#if format == "ogg":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a libvorbis {abs_file.name}.{format}"
+	#if format == "aac":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a aac {abs_file.name}.{format}"
+	#if format == "opus":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a libopus {abs_file.name}.{format}"
+	#if format == "wma":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a wmav2 {abs_file.name}.{format}"
+	#if format == "aiff":
+		#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a pcm_s16be {abs_file.name}.{format}"
+#
+	#os.system(command)
+#func change_audio_bitrate(bitrate):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -c:a libmp3lame -b:a {self.parameters[bitrate].text}k {abs_file.stem}_compressed.mp3"
+	#os.system(command)
+#func change_audio_samplerate(sample_rate):
+	#command = f"cd /D {os.path.dirname(file)} && {ffmpeg_path} -i {abs_file.name} -ar {self.parameters[sample_rate].text} {abs_file.stem}_{self.parameters[sample_rate].text}.wav"
+	#os.system(command)
+##endregion
